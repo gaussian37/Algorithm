b73ade1cc1e8b6907f0d2350f736146e219d66a1
JINSOL KIM <infoefficiency@gmail.com>
2017-06-04 21:52:34 +0900

    code
    https://www.acmicpc.net/problem/1463

---
+++ PS Code/DP/1로 만들기/code.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [int(0) for x in range(n+1)]
+
+dp[1] = 0
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    if (i % 3 == 0 and dp[i] > dp[int(i/3)] + 1):
+        dp[i] = dp[int(i/3)] + 1
+    elif(i % 2 == 0 and dp[i] > dp[int(i/2)] + 1):
+        dp[i] = dp[int(i/2)] + 1
+        
+print(dp[n])      
+        
+        
