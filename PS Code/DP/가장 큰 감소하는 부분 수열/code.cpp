a10e85e681c9f485af35b891e074153bdd74587d
JINSOL KIM <infoefficiency@gmail.com>
2017-06-16 14:13:06 +0900

    update

--- PS Code/DP/가장 큰 감소하는 부분 수열/code.cpp
+++ PS Code/DP/가장 큰 감소하는 부분 수열/code.cpp
@@ -14,15 +14,29 @@ int main() {
 		scanf("%d", &A[i]);
 	}
 
+#if 1
+	reverse(A.begin(), A.end());
+
 	for (int i = 0; i < N; ++i) {
-		dp[i] = A[i];
+		dp[i] = 1;
 		for (int j = 0; j < i; ++j) {
-			if (A[j] < A[i] && dp[i] < dp[j] + A[i]){
-				dp[i] = dp[j] + A[i];
-				//dp[i] = max(dp[i], dp[j] + A[i]);
+			if (A[j] < A[i] && dp[i] < dp[j] + 1){
+				dp[i] = dp[j] + 1;
+				//dp[i] = max(dp[i], dp[j] + 1);
 			}
 		}
 	}
+#else
+	for (int i = 0; i < N; ++i) {
+		dp[i] = 1;
+		for (int j = 0; j < i; ++j) {
+			if (A[j] > A[i] && dp[i] < dp[j] + 1) {
+				dp[i] = dp[j] + 1;
+			}
+		}
+	}
+
+#endif
 
 	int ans = *max_element(dp.begin(), dp.end());
 	printf("%d\n", ans);
