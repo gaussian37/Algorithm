f76690dbda895aa7030266ebda71fd9cbce7ce3d
JINSOL KIM <infoefficiency@gmail.com>
2017-07-17 08:28:35 +0900

    update

--- PS Code/자료구조/집합의 표현/code.cpp
+++ PS Code/자료구조/집합의 표현/code.cpp
@@ -11,9 +11,47 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vii;
 typedef pair<int, int> ii;
 
+
+vi parent;
+
+int Find(int x) {
+	if (x == parent[x]) {
+		return x;
+	}
+	else {
+		return parent[x] = Find(parent[x]);
+	}
+}
+void Union(int x, int y) {
+	x = Find(x);
+	y = Find(y);
+	if (x != y) {
+		parent[y] = x;
+	}
+}
+
 int main() {
-
-	string s;
-
-	
+	int N, M;
+	scanf("%d %d", &N, &M);
+	parent = vi(N + 10);
+	for (int i = 0; i <= N; i++) {
+		parent[i] = i;
+	}
+	while (M--) {
+		int w, x, y;
+		scanf("%d %d %d", &w, &x, &y);
+		if (w == 0) {
+			Union(x, y);
+		}
+		else {
+			x = Find(x);
+			y = Find(y);
+			if (x == y) {
+				puts("YES");
+			}
+			else {
+				puts("NO");
+			}
+		}
+	}
 }
