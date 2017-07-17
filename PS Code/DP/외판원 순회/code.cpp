e427323698f4dc504e6e3858a8f48bd74a91ba41
JINSOL KIM <infoefficiency@gmail.com>
2017-07-17 14:37:57 +0900

    update

--- PS Code/DP/외판원 순회/code.cpp
+++ PS Code/DP/외판원 순회/code.cpp
@@ -15,33 +15,32 @@ typedef pair<int, int> ii;
 int N;
 vii w, dp;
 
-int solve(int s, int i) {
-	if (s == (1 << 0) && i == 0) return 0;
-	
-	if (dp[s][i] != -1) return dp[i][i];
+int solve(int i, int j) {
+	if (i == (1 << 0) && j == 0) return 0;
 
-	dp[s][i] = 1e9;
-	for (int j = 0; j < N; ++j) {
-		if (i != j && (s&(1 << i)) && (s&(1 << j)) && w[j][i]) {
-			dp[s][i] = min(dp[s][i], dp[s - (1 << i)][j] + w[j][i]);
+	if (dp[i][j] != -1) return dp[i][j];
+
+	dp[i][j] = 1e9;
+	for (int k = 0; k < N; ++k) {
+		if (j != k && (i & (1 << j)) && (i & (1 << k)) && w[k][j]) {
+			dp[i][j] = min(dp[i][j], solve(i&~(1 << j), k) + w[k][j]);
 		}
 	}
-	return dp[s][i];
+	return dp[i][j];
 }
 int main() {
 
 	scanf("%d", &N);
 	w = vii(N + 5, vi(N + 5));
-	dp = vii(1<<20, vi(20, 1e9));
-
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
 			scanf("%d", &w[i][j]);
-
 		}
 	}
 
-#if 1
+#if 0
+
+	dp = vii((1 <<20), vi((20), 1e9));
 	dp[(1 << 0)][0] = 0;
 	for (int i = 0; i < (1 << N); ++i) {
 		for (int j = 1; j < N; ++j) {
@@ -53,14 +52,20 @@ int main() {
 		}
 	}
 
-#else
-
-
-#endif
 	int ans = 1e9;
 	for (int i = 0; i < N; ++i) {
 		ans = min(ans, dp[(1 << N) - 1][i] + w[i][0]);
 	}
+
+#else
+	dp = vii((1<<(N+2)), vi((N+2), -1));
+	int ans = 1e9;
+	for (int i = 0; i < N; ++i) {
+		ans = min(ans, solve((1 << N) - 1, i) + w[i][0]);
+	}
+
+#endif
+	
 	printf("%d\n", ans);
 
 }
