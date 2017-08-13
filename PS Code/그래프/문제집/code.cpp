15184f343a89233ea5cc1ef3b56617e634c57265
JINSOL KIM <infoefficiency@gmail.com>
2017-08-13 10:42:38 +0900

    update

---
+++ PS Code/그래프/문제집/code.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+vi AdjList[32010];
+vi ind;
+
+int main() {
+	int N, M;
+	scanf("%d %d", &N, &M);
+	ind = vi(N + 2);
+	for (int i = 0; i<M; ++i) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		AdjList[u].push_back(v);
+		ind[v] += 1;
+	}
+
+	priority_queue<int, vi, greater<int>> q;
+	for (int i = 1; i <= N; ++i) {
+		if (ind[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	for (int k = 0; k<N; k++) {
+		int u = q.top();
+		q.pop();
+		printf("%d ", u);
+
+		for (int i = 0; i<AdjList[u].size(); i++) {
+			int v = AdjList[u][i];
+			ind[v] -= 1;
+			if (ind[v] == 0) {
+				q.push(v);
+			}
+		}
+	}
+
+	return 0;
+}
