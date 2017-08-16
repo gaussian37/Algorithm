b8d1da0b1c96fabb74d0895af9d8c73d4cdec0a1
JINSOL KIM <infoefficiency@gmail.com>
2017-08-16 23:47:27 +0900

    update

--- PS Code/그래프/플로이드/code.cpp
+++ PS Code/그래프/플로이드/code.cpp
@@ -18,38 +18,29 @@ int INF = 1e9;
 int main() {
 
 	scanf("%d%d", &N, &M);
-	vii dp(N + 2, vi(N + 2));
+	vii dp(N + 2, vi(N + 2, INF));
 
 	for (int i = 0; i < M; ++i) {
 		int u, v, w;
 		scanf("%d%d%d", &u, &v, &w);
-		if (dp[u][v] == 0) {
-			dp[u][v] = w;
-		}
-		else {
-			dp[u][v] = min(dp[u][v], w);
-		}
+		dp[u][v] = min(dp[u][v], w);
+	}
+
+	for (int i = 1; i <= N; ++i) {
+		dp[i][i] = 0;
 	}
 
 	for (int k = 1; k <= N; ++k) {
 		for (int i = 1; i <= N; ++i) {
 			for (int j = 1; j <= N; ++j) {
-				if (i == j) continue;
-				if (dp[i][k] == 0 || dp[k][j] == 0) continue;
-
-				if (dp[i][j] == 0) {
-					dp[i][j] = dp[i][k] + dp[k][j];
-				}
-				else {
-					dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
-				}
+				dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
 			}
 		}
 	}
 
 	for (int i = 1; i <= N; ++i) {
 		for (int j = 1; j <= N; ++j) {
-			printf("%d ", dp[i][j]);
+			printf("%d ", dp[i][j] == INF ? 0 : dp[i][j]);
 		}puts("");
 	}
 	
