a2a44ddb25d48972635673a12738257bf29818e8
JINSOL KIM <infoefficiency@gmail.com>
2017-08-26 11:18:18 +0900

    update

--- PS Code/트리/트리의 부모 찾기/code.cpp
+++ PS Code/트리/트리의 부모 찾기/code.cpp
@@ -1,58 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <queue>
 
 using namespace std;
 
 typedef vector<int> vi;
-typedef vector<vi> vii;
-
-vii A;
-
-void preorder(int x) {
-	if (x == -1) return;
-	printf("%c", (char)(x + 'A'));
-	preorder(A[x][0]);
-	preorder(A[x][1]);
-}
-
-void inorder(int x) {
-	if (x == -1) return;
-	inorder(A[x][0]);
-	printf("%c", (char)(x + 'A'));
-	inorder(A[x][1]);
-}
-
-void postorder(int x) {
-	if (x == -1) return;
-	postorder(A[x][0]);
-	postorder(A[x][1]);
-	printf("%c", (char)(x + 'A'));
-}
+const int MAX = 100010;
+vi A[MAX];
+vi parent, visited, depth, dist;
 
 int main() {
 	int N;
 	scanf("%d", &N);
-	A = vii(N + 2, vi(2));
-	for (int i = 1; i <= N; ++i) {
-		string a, b, c;
-		cin >> a >> b >> c;		
-		if (b[0] == '.') {
-			A[a[0] - 'A'][0] = -1;
-		}
-		else {
-			A[a[0] - 'A'][0] = b[0] - 'A';
-		}
-		if (c[0] == '.') {
-			A[a[0] - 'A'][1] = -1;
-		}
-		else {
-			A[a[0] - 'A'][1] = c[0] - 'A';
+	parent = visited = depth = dist = vi(N + 2);
+	for (int i = 0; i < N - 1; i++) {
+		int u, v, w;
+		scanf("%d %d", &u, &v);
+		A[u].push_back(v);
+		A[v].push_back(u);
+	}
+
+	depth[1] = 0;
+	visited[1] = true;
+	queue<int> q;
+	q.push(1);
+	parent[1] = 0;
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		for (int y : A[x]) {
+			if (!visited[y]) {
+				depth[y] = depth[x] + 1;
+				visited[y] = true;
+				parent[y] = x;
+				q.push(y);
+			}
 		}
 	}
 
-	preorder(0); puts("");
-	inorder(0); puts("");
-	postorder(0); puts("");
-	
+	for (int i = 2; i <= N; i++) {
+		printf("%d\n", parent[i]);
+	}
+	return 0;
 }
