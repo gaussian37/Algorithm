2f9d221863dddbfda6a44685f2526a7ae84e7fd2
JINSOL KIM <infoefficiency@gmail.com>
2017-09-07 23:00:40 +0900

    update

--- Algorithm/세그먼트 트리/2D_fenwick_tree.cpp
+++ Algorithm/세그먼트 트리/2D_fenwick_tree.cpp
@@ -5,21 +5,21 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> vii;
 
-int N;
-vii tree;
-int range_sum(int x, int y) {
+int N, M;
+vii tree, A;
+int range_sum(int y, int x) {
 	int ans = 0;
-	for(int i = x; i > 0; i -= i & -i){
-		for (int j = y; j > 0; j -= j & -j) {
+	for (int i = y; i > 0; i -= i & -i) {
+		for (int j = x; j > 0; j -= j & -j) {
 			ans += tree[i][j];
 		}
 	}
 	return ans;
 }
 
-int update(int x, int y, int diff) {
-	for (int i = x; i <= N; i+= i & -i) {
-		for (int j = y; j <= N; j += j & -j) {
+void update(int y, int x, int diff) {
+	for (int i = y; i <= N; i += i & -i) {
+		for (int j = x; j <= N; j += j & -j) {
 			tree[i][j] += diff;
 		}
 	}
